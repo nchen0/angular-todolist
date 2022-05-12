02c900c5bf4b0fb67df433c07821051709126b5e
Nicky Chen <nicky.chen0@gmail.com>
2022-05-12 11:54:53 -0500

    add todos

---
+++ src/app/todos.ts
@@ -0,0 +1,13 @@
+export interface Todos {
+    id: number;
+    description: string;
+    completed: boolean;
+}
+
+export const todos = [
+    {
+        id: 1,
+        description: "Walk the dog",
+        completed: false
+    }
+]
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TodoService } from '../todo.service';
+import { todos } from '../todos';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  todos = todos;
+  checkoutForm = this.formBuilder.group({
+    description: '',
+    completed: false
+  })
+
+  constructor(private todoService: TodoService, private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    
+  }
+
+}
