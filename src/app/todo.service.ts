24d0431d7a422ae86d6e54e6442749a07d277dff
Nicky Chen <nicky.chen0@gmail.com>
2022-05-12 14:12:09 -0500

    add todos

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Todo, todos } from './todos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todos = todos;
 
   constructor() { }
+
+  addTodos(todo: Todo) {
+    console.log('adding');
+    this.todos.push(todo);
+  }
 }
--- src/app/todos.ts
+++ src/app/todos.ts
@@ -1,4 +1,4 @@
-export interface Todos {
+export interface Todo {
     id: number;
     description: string;
     completed: boolean;
